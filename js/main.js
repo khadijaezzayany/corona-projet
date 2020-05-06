7a81c00b6202f38330e8f3713cf847d71100dd2c
khadijaezzayany <khadijaezzayany8@gmail.com>
2020-05-06 12:08:14 +0000

    fixed input-answer

---
+++ js/main.js
@@ -0,0 +1,417 @@
+//////////////////// Selector
+const startBut = document.querySelector('#startBut');
+const préambule = document.querySelector('.préambule');
+const questionnaire = document.querySelector('.questionnaire');
+const stepper = document.querySelectorAll('.stepper h1');
+const previousBut = document.querySelector('.previous');
+const nextBut = document.querySelector('.next');
+const questionElement = document.querySelector('.question');
+const answerInputs = document.querySelector('.answer-inputs');
+const progressBar = document.querySelector('.bar');
+const questionNumber = document.querySelector('.question__number');
+
+
+
+
+//////////////////// Event
+
+startBut.addEventListener('click', startTest);
+
+
+answerInputs.addEventListener('change', (event) => {
+    const input = event.target;
+    if (input.type === "radio") {
+        nextBut.disabled = false;
+    } else {
+        nextBut.disabled = false;
+    };
+});
+
+
+
+
+//////////////////// Function
+
+let currentQuestionIndex = 0;
+
+//:::::::::::::::::hideprevious
+
+function hidepreviousBut() {
+    if (currentQuestionIndex === 0) {
+        previousBut.classList.add('hide');
+    } else {
+        previousBut.classList.remove('hide');
+    }
+}
+
+//:::::::::::::::::Start test
+
+function startTest() {
+    startBut.style.display = 'none';
+    préambule.style.display = 'none';
+    questionnaire.style.display = 'block';
+    stepper[0].classList.remove('select');
+    stepper[1].classList.add('select');
+    showQuestion(questions[currentQuestionIndex]);
+    hidepreviousBut();
+    nextBut.disabled = true;
+
+
+}
+//:::::::::::::::::show next but
+
+nextBut.addEventListener('click', () => {
+    if (currentQuestionIndex < 21) {
+        currentQuestionIndex++;
+        showQuestion(questions[currentQuestionIndex]);
+        progress(currentQuestionIndex);
+        hidepreviousBut();
+        nextBut.disabled = true;
+        if (currentQuestionIndex === 21) {
+            nextBut.innerText = 'Terminer le test';
+        } else {
+            nextBut.innerText = 'Suivant';
+        }
+    }
+})
+
+//:::::::::::::::::show previous but
+previousBut.addEventListener('click', () => {
+    currentQuestionIndex--;
+    showQuestion(questions[currentQuestionIndex])
+    progress(currentQuestionIndex);
+    hidepreviousBut();
+    nextBut.disabled = true;
+    if (currentQuestionIndex === 21) {
+        nextBut.innerText = 'Terminer le test';
+
+    } else {
+        nextBut.innerText = 'Suivant';
+    }
+})
+
+//:::::::::::::::::show Question
+
+
+function showQuestion(question) {
+    questionElement.innerHTML = question.question;
+    answerInputs.innerHTML = '';
+    const inputAnswer = question.input.answer;
+    const input = question.input;
+    if (question.input.type === 'radio') {
+        inputAnswer.forEach((answer) => {
+            answerInputs.innerHTML += `
+                    <div>
+                        <input type="radio" name="${input.qNumber}" id="${answer.text}">
+                        <label for="${answer.text}">
+                        <span>${answer.text}</span> </label>
+                    </div>`;
+        });
+    } else {
+        answerInputs.innerHTML += `<input type="number" name="${input.qNumber}" id="${input.name}" min="${input.min}" max="${input.max}" placeholder="${input.min} - ${input.max}">
+                                    <span class="input-span">${input.name}</span>`;
+    }
+
+}
+
+
+
+
+//:::::::::::::::::folowProgress
+
+function progress(number) {
+    const currentNmber = number + 1;
+
+    questionNumber.innerText = currentNmber;
+    progressBar.style.width = `calc(${currentNmber} * calc(100% / 22))`;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const questions = [{
+        question: 'Pensez-vous avoir ou avoir eu de la fièvre ces 10 derniers jours (frissons, sueurs) ?',
+
+        input: {
+            type: 'radio',
+            qNumber: 'Q1',
+            answer: [{
+                text: 'Oui',
+            }, {
+                text: 'Non',
+            }]
+        }
+    }, {
+        question: 'Quelle est votre température corporelle ?',
+
+        input: {
+            type: 'number',
+            qNumber: 'Q2',
+            name: 'degrés',
+            min: 34,
+            max: 42
+        }
+    }, {
+        question: 'Ces derniers jours, avez-vous une toux ou une augmentation de votre toux habituelle ?',
+
+        input: {
+            type: 'radio',
+            qNumber: 'Q3',
+            answer: [{
+                text: 'Oui',
+            }, {
+                text: 'Non',
+            }]
+        }
+    }, {
+        question: 'Avez-vous eu des courbatures inhabituelles au cours des derniers jours ?',
+
+        input: {
+            type: 'radio',
+            qNumber: 'Q4',
+            answer: [{
+                text: 'Oui',
+            }, {
+                text: 'Non',
+            }]
+        }
+    }, {
+        question: 'Ces derniers jours, avez-vous un mal de gorge ?',
+
+        input: {
+            type: 'radio',
+            qNumber: 'Q5',
+            answer: [{
+                text: 'Oui',
+            }, {
+                text: 'Non',
+            }]
+        }
+    }, {
+        question: 'Ces dernières 24 heures, avez-vous de la diarrhée ? Avec au moins 3 selles molles.',
+
+        input: {
+            type: 'radio',
+            qNumber: 'Q6',
+            answer: [{
+                text: 'Oui',
+            }, {
+                text: 'Non',
+            }]
+        }
+    }, {
+        question: 'Ces derniers jours, avez-vous une fatigue inhabituelle qui vous a obligé à vous reposer plus de la moitié de la journée ?',
+
+        input: {
+            type: 'radio',
+            qNumber: 'Q7',
+            answer: [{
+                text: 'Oui',
+            }, {
+                text: 'Non',
+            }]
+        }
+    }, {
+        question: 'Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ?',
+
+        input: {
+            type: 'radio',
+            qNumber: 'Q8',
+            answer: [{
+                text: 'Oui',
+            }, {
+                text: 'Non',
+            }]
+        }
+    }, {
+        question: 'Dans les dernières 24 heures, avez-vous noté un manque de souffle inhabituel lorsque vous parlez ou faites un petit effort ?',
+
+        input: {
+            type: 'radio',
+            qNumber: 'Q9',
+            answer: [{
+                text: 'Oui',
+            }, {
+                text: 'Non',
+            }]
+        }
+    }, {
+        question: 'Actuellement, comment vous vous sentez ?',
+
+        input: {
+            type: 'radio',
+            qNumber: 'Q10',
+            answer: [{
+                text: 'Bien',
+            }, {
+                text: 'Assez bien',
+            }, {
+                text: 'Fatigué(e)',
+            }, {
+                text: 'Très fatigué',
+            }]
+        }
+    }, {
+        question: 'Quel est votre âge ? Ceci, afin de calculer un facteur de risque spécifique.',
+
+        input: {
+
+            type: 'number',
+            qNumber: 'Q11',
+            name: 'ans',
+            min: 15,
+            max: 110
+        }
+    }, {
+        question: 'Quel est votre poids ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.',
+
+        input: {
+            type: 'number',
+            qNumber: 'Q12',
+            name: 'kg',
+            min: 20,
+            max: 250
+        }
+    }, {
+        question: 'Quelle est votre taille ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.',
+
+        input: {
+            type: 'number',
+            qNumber: 'Q13',
+            name: 'cm',
+            min: 80,
+            max: 250
+        }
+    }, {
+        question: 'Avez-vous de l’hypertension artérielle mal équilibrée ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez-vous un traitement à visée cardiologique ?',
+
+        input: {
+            type: 'radio',
+            qNumber: 'Q14',
+            answer: [{
+                text: 'Oui',
+            }, {
+                text: 'Non',
+            }]
+        }
+    }, {
+        question: 'Êtes-vous diabétique ?',
+
+        input: {
+            type: 'radio',
+            qNumber: 'Q15',
+            answer: [{
+                text: 'Oui',
+            }, {
+                text: 'Non',
+            }]
+        }
+    }, {
+        question: 'Avez-vous ou avez-vous eu un cancer ?',
+
+        input: {
+            type: 'radio',
+            qNumber: 'Q16',
+            answer: [{
+                text: 'Oui',
+            }, {
+                text: 'Non',
+            }]
+        }
+    }, {
+        question: 'Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ?',
+
+        input: {
+            type: 'radio',
+            qNumber: 'Q17',
+            answer: [{
+                text: 'Oui',
+            }, {
+                text: 'Non',
+            }]
+        }
+    }, {
+        question: 'Avez-vous une insuffisance rénale chronique dialysée ?',
+
+        input: {
+            type: 'radio',
+            qNumber: 'Q18',
+            answer: [{
+                text: 'Oui',
+            }, {
+                text: 'Non',
+            }]
+        }
+    }, {
+        question: 'Avez-vous une maladie chronique du foie ?',
+
+        input: {
+            type: 'radio',
+            qNumber: 'Q19',
+            answer: [{
+                text: 'Oui',
+            }, {
+                text: 'Non',
+            }]
+        }
+    }, {
+        question: 'Êtes-vous enceinte ?',
+
+        input: {
+            type: 'radio',
+            qNumber: 'Q20',
+            answer: [{
+                text: 'Oui',
+            }, {
+                text: 'Non',
+            }, {
+                text: 'Homme',
+
+            }]
+        }
+    }, {
+        question: 'Avez-vous une maladie connue pour diminuer vos défenses immunitaires ?',
+
+        input: {
+            type: 'radio',
+            qNumber: 'Q21',
+            answer: [{
+                text: 'Oui',
+            }, {
+                text: 'Non',
+            }]
+        }
+    }, {
+        question: 'Prenez-vous un traitement immunosuppresseur ? C’est un traitement qui diminue vos défenses contre les infections. Voici quelques exemples : corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste non exhaustive).',
+
+        input: {
+            type: 'radio',
+            qNumber: 'Q22',
+            answer: [{
+                text: 'Oui',
+            }, {
+                text: 'Non',
+            }]
+        }
+    }
+
+]
